0a438a2eaaa24d1cb9123b188aeaddc6346b8da9
David Pham <email@davidpham.ca>
2018-06-27 17:00:08 -0400

    Completed HW Assignment

---
+++ 04-jquery/Star Wars RPG/README.md
@@ -0,0 +1,16 @@
+# Star Wars RPG
+
+Take control of one of four Star Wars characters and battle your way to victory.
+
+## How do I play?
+
+Click on a character, click on a enemy then attack!
+
+## Built With
+
+- [Bootstrap](https://getbootstrap.com)
+- [jQuery](http://www.jquery.com)
+
+## Author
+
+David Pham
--- 04-jquery/Star Wars RPG/assets/javascript/game.js
+++ 04-jquery/Star Wars RPG/assets/javascript/game.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
-  var attackingCharacter, defendingCharacter;
+  var attackingCharacter;
+  var defendingCharacter;
   var selectedCharacter;
   var currentEnemy;
+  var enemiesRemaining = 3;
 
+  // Default state of reset button: hidden
+  $('#reset-button').hide();
+
+  // Take a copy of the current DOM for reset
+  var reset = $('#reset').clone();
+
+  // Define playable character objects
   var characters = [
     {
       id: 'darth-maul',
@@ -23,7 +32,7 @@ $(document).ready(function() {
     {
       id: 'obiwan-kenobi',
       name: 'Obi-Wan Kenobi',
-      hp: 120,
+      hp: 145,
       ap: 8,
       baseap: 8,
       cap: 4
@@ -31,160 +40,219 @@ $(document).ready(function() {
     {
       id: 'luke-skywalker',
       name: 'Luke Skywalker',
-      hp: 100,
+      hp: 135,
       ap: 10,
       baseap: 10,
       cap: 5
     }
   ];
 
+  // Attack
   function Attack(attackingCharacter, defendingCharacter) {
+    // Decrease attacking characters hp by defending characters counter ap
     attackingCharacter.hp -= defendingCharacter.cap;
+
+    // Decrease defending characters hp by attacking characters ap
     defendingCharacter.hp -= attackingCharacter.ap;
+
+    // Increase attacking characters "experience" by increasing ap by base ap
     attackingCharacter.ap += attackingCharacter.baseap;
-    console.log("Attacking Character's HP:" + attackingCharacter.hp);
-    console.log("Attacking Character's AP:" + attackingCharacter.ap);
-    console.log("Defending Character's HP: " + defendingCharacter.hp);
-    return 'FIGHT!';
-  }
 
-  function Character(name, hp, ap, cap) {
-    // Character Name
-    this.name = name;
+    // Check to win-lose conditions
+    if (enemiesRemaining > 0) {
+      if (attackingCharacter.hp <= 0) {
+        $('#status').text('You have been defeated...GAME OVER!');
 
-    // Hit Points
-    this.hp = hp;
+        // Make reset button available
+        $('#reset-button').show();
+      } else if (defendingCharacter.hp <= 0) {
+        // Reduce enemy count by 1
+        enemiesRemaining -= 1;
+        console.log('Enemies Remaining: ' + enemiesRemaining);
 
-    // Attack Power
-    this.ap = ap;
+        if (enemiesRemaining <= 0) {
+          $('#status').text('YOU WON!!!! GAME OVER!!!');
 
-    // Base Attack Power
-    this.baseap = ap;
+          // Remove defeated character from the game
+          $('#' + defendingCharacter.id).remove();
+          currentEnemy = '';
 
-    // Counter Attack Power
-    this.cap = cap;
+          // Make reset button available
+          $('#reset-button').show();
+        } else {
+          $('#status').text(
+            'You have defeated ' +
+              defendingCharacter.name +
+              ', you can choose to fight another enemy.'
+          );
 
-    // Attack
-    this.attack = function() {
-      // Deduct counter attack damage from hp
-      /*  this.hp -= cad; */
+          // Remove defeated character from the game
+          $('#' + defendingCharacter.id).remove();
+          currentEnemy = '';
+        }
+      } else {
+        $('#status').text(
+          'You attacked ' +
+            defendingCharacter.name +
+            ' for ' +
+            (attackingCharacter.ap - attackingCharacter.baseap) +
+            ' damage. ' +
+            defendingCharacter.name +
+            ' attacked you back for ' +
+            defendingCharacter.cap +
+            ' damage.'
+        );
 
-      // Increase ap by base ap;
-      this.ap += this.baseAP;
+        // Update attacking characters hp
+        $('#' + attackingCharacter.id + '-hp').text(
+          'HP: ' + attackingCharacter.hp
+        );
 
-      switch (currentEnemy) {
-        case 'luke-skywalker':
-          lukeSkywalker.hp -= this.ap;
-          this.hp -= lukeSkywalker.cap;
-          break;
-        case 'darth-maul':
-          darthMaul -= this.ap;
-          this.hp -= darthMaul.cap;
-          break;
-        case 'obiwan-kenobi':
-          obiWanKenobi -= this.ap;
-          this.hp -= obiWanKenobi.cap;
-          break;
-        case 'darth-sidious':
-          darthSidious -= this.ap;
-          this.hp -= darthSidious.cap;
-          break;
-        default:
-          console.log(this.hp);
+        // Update defending characters hp
+        $('#' + defendingCharacter.id + '-hp').text(
+          'HP: ' + defendingCharacter.hp
+        );
       }
-      console.log(lukeSkywalker.hp);
-    };
-
-    this.createCharacter = function() {
-      var characterCard = $('<div>');
-      characterCard.addClass('card');
-      characterCard.attr('style', 'width: 18rem;');
-      characterCard.text(this.name);
-      /*   
-    <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src=".../100px180/?text=Image cap" alt="Card image cap">
-        <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-    </div>
-    */
-    };
+    }
   }
 
+  // Select character
   function SelectCharacter(characterID) {
+    // Check to see if character is already selected
     if (!selectedCharacter) {
+      // Assign the selected character
       selectedCharacter = characterID;
 
+      // Loop through all characters and assign all but the selected characters as enemies
       for (var i = 0; i < characters.length; i++) {
         if (characters[i].id !== selectedCharacter) {
           $('#' + characters[i].id).appendTo('#enemies');
+
+          // Change background to red
+          $('#' + characters[i].id).addClass('bg-danger');
         }
       }
     } else if (!currentEnemy && selectedCharacter !== characterID) {
+      // Move selected character to defender slot
       $('#' + characterID).appendTo('#defender');
+      $('#' + characterID).addClass('bg-dark');
+
+      // Set the current defender
       currentEnemy = characterID;
+
+      // Clear any status text
+      $('#status').text('');
     }
   }
 
-  // Create Characters
-  darthMaul = new Character('Darth Maul', 180, 6, 25);
-  darthSidious = new Character('Darth Sidious', 150, 5, 20);
-  obiWanKenobi = new Character('Obi-Wan Kenobi', 120, 8, 4);
-  lukeSkywalker = new Character('Luke Skywalker', 100, 10, 5);
+  // Respond to character click
+  function AssignClick() {
+    $('.character').click(function() {
+      SelectCharacter(this.id);
+    });
+  }
 
-  $('#luke-skywalker').click(function() {
-    SelectCharacter(this.id);
-  });
+  AssignClick();
 
-  $('#darth-maul').click(function() {
-    SelectCharacter(this.id);
-  });
+  // Reset the game
+  function AssignReset() {
+    $('#reset-button').click(function() {
+      // Reset characters to their initial state
+      characters = [
+        {
+          id: 'darth-maul',
+          name: 'Darth Maul',
+          hp: 180,
+          ap: 6,
+          baseap: 6,
+          cap: 25
+        },
+        {
+          id: 'darth-sidious',
+          name: 'Darth Sidious',
+          hp: 150,
+          ap: 5,
+          baseap: 5,
+          cap: 20
+        },
+        {
+          id: 'obiwan-kenobi',
+          name: 'Obi-Wan Kenobi',
+          hp: 145,
+          ap: 8,
+          baseap: 8,
+          cap: 4
+        },
+        {
+          id: 'luke-skywalker',
+          name: 'Luke Skywalker',
+          hp: 135,
+          ap: 10,
+          baseap: 10,
+          cap: 5
+        }
+      ];
 
-  $('#obiwan-kenobi').click(function() {
-    SelectCharacter(this.id);
-  });
+      // Reset all variables to their initial state
+      attackingCharacter = undefined;
+      defendingCharacter = undefined;
+      selectedCharacter = undefined;
+      currentEnemy = undefined;
+      enemiesRemaining = 3;
 
-  $('#darth-sidious').click(function() {
-    SelectCharacter(this.id);
-  });
+      // Reset DOM to initial state
+      $('#reset').replaceWith(reset.clone());
 
-  $('#fighting').click(function() {
-    switch (selectedCharacter) {
-      case 'darth-maul':
-        attackingCharacter = characters[0];
-        break;
-      case 'darth-sidious':
-        attackingCharacter = characters[1];
-        break;
-      case 'obiwan-kenobi':
-        attackingCharacter = characters[2];
-        break;
-      case 'luke-skywalker':
-        attackingCharacter = characters[3];
-        break;
-    }
+      // Assign event handlers to objects
+      AssignClick();
+      AssignFight();
+      AssignReset();
+    });
+  }
 
-    switch (currentEnemy) {
-      case 'darth-maul':
-        defendingCharacter = characters[0];
-        break;
-      case 'darth-sidious':
-        defendingCharacter = characters[1];
-        break;
-      case 'obiwan-kenobi':
-        defendingCharacter = characters[2];
-        break;
-      case 'luke-skywalker':
-        defendingCharacter = characters[3];
-        break;
-    }
+  AssignReset();
 
-    console.log(Attack(attackingCharacter, defendingCharacter));
+  // Fight
+  function AssignFight() {
+    $('#fighting').click(function() {
+      // Assign the attacking character
+      switch (selectedCharacter) {
+        case 'darth-maul':
+          attackingCharacter = characters[0];
+          break;
+        case 'darth-sidious':
+          attackingCharacter = characters[1];
+          break;
+        case 'obiwan-kenobi':
+          attackingCharacter = characters[2];
+          break;
+        case 'luke-skywalker':
+          attackingCharacter = characters[3];
+          break;
+      }
 
-    if (defendingCharacter.hp <= 0) {
-      alert('You win!');
-    } else if (attackingCharacter.hp <= 0) {
-      alert('You lose!');
-    }
-  });
+      // Assign the defending character
+      switch (currentEnemy) {
+        case 'darth-maul':
+          defendingCharacter = characters[0];
+          break;
+        case 'darth-sidious':
+          defendingCharacter = characters[1];
+          break;
+        case 'obiwan-kenobi':
+          defendingCharacter = characters[2];
+          break;
+        case 'luke-skywalker':
+          defendingCharacter = characters[3];
+          break;
+      }
+
+      // Attack
+      if (currentEnemy) {
+        Attack(attackingCharacter, defendingCharacter);
+      }
+    });
+  }
+
+  AssignFight();
 });
