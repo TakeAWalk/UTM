555da0be7e2c371cce5880c054fbf0c45699067e
David Pham <email@davidpham.ca>
2018-07-06 13:53:41 -0400

    Completed app and bootstrap elements

--- 05-timers/assets/javascript/app.js
+++ 05-timers/assets/javascript/app.js
@@ -1,14 +1,20 @@
 $(document).ready(function() {
-  // Time limit per question
-  let timeLimit = 6;
-  let answerDisplayTime = 5;
+  // Time to answer each question
+  let timeLimit = 10;
 
+  // Time to display question results
+  let answerDisplayTime = 3;
+
+  // Record the current question
   var currentQuestion = 0;
+
+  // Used to determine if the game is in a result pause state or if the time ran out
   var paused = false;
 
   // Take a copy of the current DOM for reset
   var reset = $('#container').clone();
 
+  // Performance counter
   var performance = {
     correct: 0,
     incorrect: 0,
@@ -36,12 +42,13 @@ $(document).ready(function() {
         'A shield'
       ],
       rightAnswer: 2
+    },
+    {
+      question: 'In the show New Girl, who does Jess date?',
+      answers: ['Nick Miller', 'Coach', 'Winston Bishop', 'Max Bayfield'],
+      rightAnswer: 1
     }
   ];
-
-  /* 
-      Reusing the stopwatch code from our class activity
-  */
 
   //  Variable that will hold our setInterval that runs the stopwatch
   var intervalId;
@@ -62,6 +69,8 @@ $(document).ready(function() {
     start: function(elapsed) {
       // DONE: Use setInterval to start the count here and set the clock to running.
       stopwatch.time = elapsed;
+      // DONE: Change the "display" div "
+      $('#display').text('Time Remaining: ' + stopwatch.time + ' Seconds');
       if (!clockRunning) {
         intervalId = setInterval(stopwatch.count, 1000);
         clockRunning = true;
@@ -81,7 +90,6 @@ $(document).ready(function() {
 
       if (stopwatch.time === 0) {
         if (!paused) {
-          alert('unanswered');
           performance.unanswered++;
         }
         stopwatch.stop();
@@ -94,20 +102,25 @@ $(document).ready(function() {
   };
 
   function NextQuestion() {
+    // Start the clock
     stopwatch.start(timeLimit);
 
+    // Check to see if there are any more questions
     if (currentQuestion < triviaQuestions.length) {
-      $('#trivia_question').text(triviaQuestions[currentQuestion].question);
+      // Assign the text to the question and answers
+      $('#trivia-question').text(triviaQuestions[currentQuestion].question);
       $('#answer-1').text(triviaQuestions[currentQuestion].answers[0]);
       $('#answer-2').text(triviaQuestions[currentQuestion].answers[1]);
       $('#answer-3').text(triviaQuestions[currentQuestion].answers[2]);
       $('#answer-4').text(triviaQuestions[currentQuestion].answers[3]);
 
+      // Unbind any events to the existing buttons
       $('#answer-1').unbind();
       $('#answer-2').unbind();
       $('#answer-3').unbind();
       $('#answer-4').unbind();
 
+      // Assign the right and wrong events to the apporpirate buttons
       if (triviaQuestions[currentQuestion].rightAnswer === 1) {
         $('#answer-1').click(Right);
         $('#answer-2').click(Wrong);
@@ -129,24 +142,42 @@ $(document).ready(function() {
         $('#answer-3').click(Wrong);
         $('#answer-4').click(Right);
       }
+
+      // Increment the current question
       currentQuestion++;
     } else {
+      // If there are no more questions, end the game
       GameOver();
     }
   }
 
+  // Display the gamers performance
   function GameOver() {
+    // Stop the clock
     stopwatch.stop();
+
+    // Hide the questions
     $('#question').hide();
 
+    // Show and update the results
     $('#result').show();
     $('#correct').text('Correct Answers: ' + performance.correct);
     $('#incorrect').text('Incorrect Answers: ' + performance.incorrect);
     $('#unanswered').text('Unanswered: ' + performance.unanswered);
+
+    // Reset the counters
+    currentQuestion = 0;
+    performance.correct = 0;
+    performance.incorrect = 0;
+    performance.unanswered = 0;
   }
 
+  // User got the question wrong
   function Wrong() {
+    // Hide the questions
     $('#question').hide();
+
+    // Show the question results
     $('#question-result').show();
     $('#right-wrong').text('Nope!');
     $('#right-wrong-text').text(
@@ -156,59 +187,68 @@ $(document).ready(function() {
         ]
     );
 
+    //S top the clock
     stopwatch.stop();
+
+    // Update the performance counter
     performance.incorrect++;
 
+    // Set status to pause so it doesn't count as an unanswered question
     paused = true;
-    stopwatch.start(5);
+
+    // Start the clock to show the results
+    stopwatch.start(answerDisplayTime);
   }
 
+  // User got the question right
   function Right() {
     $('#question').hide();
     $('#question-result').show();
     $('#right-wrong').text('Correct!');
     $('#right-wrong-text').text('');
 
+    // Stock the clock
     stopwatch.stop();
+
+    // Update the performance counter
     performance.correct++;
 
+    // Set status to pause so it doesn't count as an unanswered question
     paused = true;
-    stopwatch.start(5);
+
+    // Start the clock to show the results
+    stopwatch.start(answerDisplayTime);
   }
 
-  $('#question').hide();
-  $('#result').hide();
-
-  $('#start').on('click', function() {
-    $('#question').show();
-    $('#start').hide();
-    stopwatch.start();
-
-    NextQuestion();
-  });
-
-  function Reset() {
-    currentQuestion = 0;
-    performance.correct = 0;
-    performance.uncorrect = 0;
-    performance.unanswered = 0;
-
-    // Reset DOM to initial state
-    $('#container').replaceWith(reset.clone());
-    stopwatch.reset();
-    $('#question').hide();
-    $('#result').hide();
-
-    $('#reset').click(Reset);
-
+  function AssignStart() {
     $('#start').on('click', function() {
       $('#question').show();
       $('#start').hide();
-      stopwatch.start(timeLimit);
 
       NextQuestion();
     });
   }
 
-  $('#reset').click(Reset);
+  function AssignReset() {
+    $('#reset').click(Reset);
+  }
+
+  // When the document initally loads, hide the questions and results
+  $('#question').hide();
+  $('#result').hide();
+
+  AssignStart();
+  AssignReset();
+
+  function Reset() {
+    // Reset DOM to initial state
+    $('#container').replaceWith(reset.clone());
+    // stopwatch.reset();
+    $('#question').hide();
+    $('#result').hide();
+
+    $('#reset').click(Reset);
+
+    AssignStart();
+  }
 });
