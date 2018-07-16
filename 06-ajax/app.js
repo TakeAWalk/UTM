290472dd126b46fc22aa5f0bf70e9f188b09e8f4
David Pham <email@davidpham.ca>
2018-07-16 10:47:53 -0400

    worked on hw

--- 06-ajax/app.js
+++ 06-ajax/app.js
@@ -1,4 +1,4 @@
-/* API Key: kVEh3AuPwGkBkRK9x540E5QC5E7RGekm */
+var apiKey = 'kVEh3AuPwGkBkRK9x540E5QC5E7RGekm';
 
 var topics = [
   'Anne',
@@ -19,7 +19,11 @@ function createButtons(elementID) {
   for (var i = 0; i < topics.length; i++) {
     //Create buttons for each index
     $('#' + elementID).append(
-      '<button id = ' + i + '>' + topics[i] + '</button>'
+      '<button type="button" class = "btn btn-primary" id = ' +
+        i +
+        '>' +
+        topics[i] +
+        '</button>'
     );
 
     //Assign click function to each index
@@ -35,15 +39,32 @@ function createButtons(elementID) {
 function populateImages(response) {
   //TODO: Grab 10 static non-animated gif images from the GIPHY API and place it on this page
   for (var i = 0; i < response.data.length; i++) {
-    console.log(response.data[i].images.downsized_still.url);
-    $('#images').append(
-      '<p>Rating: ' +
-        response.data[i].rating +
-        '<br /><img src="' +
-        response.data[i].images.downsized_still.url +
-        '" class="img-fluid"></img></p>'
-    );
+    $('#images').append('Rating: ' + response.data[i].rating);
+
+    var giphyImg = $('<img>');
+    giphyImg.attr('src', response.data[i].images.downsized_still.url);
+    giphyImg.attr('data-still', response.data[i].images.fixed_height_still.url);
+    giphyImg.attr('data-animate', response.data[i].images.fixed_height.url);
+    giphyImg.attr('data-state', 'still');
+    giphyImg.addClass('gif');
+
+    $('#images').append(giphyImg);
   }
+
+  $('.gif').on('click', function() {
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    var state = $(this).attr('data-state');
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === 'still') {
+      $(this).attr('src', $(this).attr('data-animate'));
+      $(this).attr('data-state', 'animate');
+    } else {
+      $(this).attr('src', $(this).attr('data-still'));
+      $(this).attr('data-state', 'still');
+    }
+  });
 }
 
 function queryGiphy(callingElement) {
@@ -51,7 +72,9 @@ function queryGiphy(callingElement) {
   var queryURL =
     'http://api.giphy.com/v1/gifs/search?q=' +
     topics[callingElement] +
-    '&api_key=kVEh3AuPwGkBkRK9x540E5QC5E7RGekm&limit=10';
+    '&api_key=' +
+    apiKey +
+    '&limit=10';
 
   //Make API call to Giphy
   $.ajax({
@@ -63,7 +86,18 @@ function queryGiphy(callingElement) {
   });
 }
 
+function clearButtons() {
+  $('#buttons').html('');
+}
+
 $(document).ready(function() {
   //TODO:
   createButtons('buttons');
+
+  $('button').click(function() {
+    console.log($('#addButton').val());
+    topics.push($('#addButton').val());
+    clearButtons();
+    createButtons('buttons');
+  });
 });
