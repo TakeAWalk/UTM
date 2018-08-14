ed6adb88c8765739c413e1f6bfa9faf373793083
David Pham <david.pham@medela.ca>
2018-08-14 14:08:00 -0400

    Completed my-tweets function

--- 10-NodeJS/liri-node-app/liri.js
+++ 10-NodeJS/liri-node-app/liri.js
@@ -2,8 +2,12 @@
 require("dotenv").config();
 
 /* 9. Add the code required to import the `keys.js` file and store it in a variable. */
+const keys = require("./keys");
 
 /* * You should then be able to access your keys information like so */
+var Spotify = require("node-spotify-api");
+var Twitter = require("twitter");
+
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
@@ -14,20 +18,12 @@ var client = new Twitter(keys.twitter);
     * `do-what-it-says` */
 switch (process.argv[2]) {
   case "my-tweets":
-    /*  1. `node liri.js my-tweets`
-    
-    * This will show your last 20 tweets and when they were created at in your terminal/bash window. */
+    /*  1. `node liri.js my-tweets` */
+    myTweets();
     break;
   case "spotify-this-song":
-    /*  2. `node liri.js spotify-this-song '<song name here>'`
-
-    * This will show the following information about the song in your terminal/bash window
-        * Artist(s)
-        * The song's name
-        * A preview link of the song from Spotify
-        * The album that the song is from */
-
-    /*    * If no song is provided then your program will default to "The Sign" by Ace of Base. */
+    /*  2. `node liri.js spotify-this-song '<song name here>'` */
+    spotifyThisSong(process.argv[3]);
     break;
   case "movie-this":
     /*  3. `node liri.js movie-this '<movie name here>'`
@@ -55,6 +51,47 @@ switch (process.argv[2]) {
      
         * Feel free to change the text in that document to test out the feature for other commands. */
     break;
+}
+
+/* This will show your last 20 tweets and when they were created at in your terminal/bash window. */
+function myTweets() {
+  var params = {
+    screen_name: "@davidpham",
+    count: 20
+  };
+  client.get("statuses/user_timeline", params, function(
+    error,
+    tweets,
+    response
+  ) {
+    if (!error) {
+      for (element in tweets) {
+        console.log(tweets[element].text);
+      }
+    }
+  });
+}
+
+/* This will show the following information about the song in your terminal/bash window
+        * Artist(s)
+        * The song's name
+        * A preview link of the song from Spotify
+        * The album that the song is from */
+
+/*    * If no song is provided then your program will default to "The Sign" by Ace of Base. */
+function spotifyThisSong(songName = "The Sign") {
+  console.log(songName);
+
+  spotify.search({ type: "track", query: "All the Small Things" }, function(
+    err,
+    data
+  ) {
+    if (err) {
+      return console.log("Error occurred: " + err);
+    }
+
+    console.log(data.items);
+  });
 }
 
 /* ### BONUS
