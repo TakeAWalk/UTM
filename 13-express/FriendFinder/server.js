450f7ead71d07799a9b2b609d91c342e77ff1a7a
David Pham <email@davidpham.ca>
2018-09-08 10:43:36 -0400

    Created Friends object, coded compare method

--- 13-express/FriendFinder/server.js
+++ 13-express/FriendFinder/server.js
@@ -1,3 +1,23 @@
-import * from 'express';
-import * from 'body-parser';
-import * from 'path';
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+function Friend(name, photo, scores) {
+  this.name = name;
+  this.photo = photo;
+  this.scores = scores;
+
+  this.compareScores = friend => {
+    var acc = 0;
+    this.scores.forEach(
+      (currentValue, currentIndex) =>
+        (acc += Math.abs(currentValue - friend.scores[currentIndex]))
+    );
+    return acc;
+  };
+}
+
+var friendA = new Friend('Charlie', 'no photo', [4, 3, 4]);
+var friendB = new Friend('Donald', 'no photo', [10, 2, 3]);
+
+console.log(friendA.compareScores(friendB));
