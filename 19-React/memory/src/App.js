fa59695babc28f8b14177d585b00653c54d08285
David Pham <david.pham@medela.ca>
2018-10-26 12:09:45 -0400

    updated readme

--- 19-React/memory/src/App.js
+++ 19-React/memory/src/App.js
@@ -7,78 +7,76 @@ import Navbar from "./components/navbar";
 import Container from "./components/container";
 import Footer from "./components/footer";
 
-const defaultCards = [
-  {
-    id: 1,
-    brand: "fab fa-cc-paypal fa-10x",
-    clicked: false
-  },
-  {
-    id: 2,
-    brand: "fab fa-android fa-10x",
-    clicked: false
-  },
-  {
-    id: 3,
-    brand: "fab fa-amazon-pay fa-10x",
-    clicked: false
-  },
-  {
-    id: 4,
-    brand: "fab fa-apple-pay fa-10x",
-    clicked: false
-  },
-  {
-    id: 5,
-    brand: "fab fa-cc-stripe fa-10x",
-    clicked: false
-  },
-  {
-    id: 6,
-    brand: "fab fa-google fa-10x",
-    clicked: false
-  },
-  {
-    id: 7,
-    brand: "fab fa-js fa-10x",
-    clicked: false
-  },
-  {
-    id: 8,
-    brand: "fab fa-linkedin fa-10x",
-    clicked: false
-  },
-  {
-    id: 9,
-    brand: "fab fa-linkedin-in fa-10x",
-    clicked: false
-  },
-  {
-    id: 10,
-    brand: "fab fa-github fa-10x",
-    clicked: false
-  },
-  {
-    id: 11,
-    brand: "fab fa-npm fa-10x",
-    clicked: false
-  },
-  {
-    id: 12,
-    brand: "fab fa-node fa-10x",
-    clicked: false
-  }
-];
-
 class App extends Component {
   state = {
     counter: 0,
     highScore: 0,
     statusText: "Click an image to begin!",
-    cards: defaultCards
+    cards: [
+      {
+        id: 1,
+        brand: "fab fa-cc-paypal fa-10x",
+        clicked: false
+      },
+      {
+        id: 2,
+        brand: "fab fa-android fa-10x",
+        clicked: false
+      },
+      {
+        id: 3,
+        brand: "fab fa-amazon-pay fa-10x",
+        clicked: false
+      },
+      {
+        id: 4,
+        brand: "fab fa-apple-pay fa-10x",
+        clicked: false
+      },
+      {
+        id: 5,
+        brand: "fab fa-cc-stripe fa-10x",
+        clicked: false
+      },
+      {
+        id: 6,
+        brand: "fab fa-google fa-10x",
+        clicked: false
+      },
+      {
+        id: 7,
+        brand: "fab fa-js fa-10x",
+        clicked: false
+      },
+      {
+        id: 8,
+        brand: "fab fa-linkedin fa-10x",
+        clicked: false
+      },
+      {
+        id: 9,
+        brand: "fab fa-linkedin-in fa-10x",
+        clicked: false
+      },
+      {
+        id: 10,
+        brand: "fab fa-github fa-10x",
+        clicked: false
+      },
+      {
+        id: 11,
+        brand: "fab fa-npm fa-10x",
+        clicked: false
+      },
+      {
+        id: 12,
+        brand: "fab fa-node fa-10x",
+        clicked: false
+      }
+    ]
   };
 
-  handleIncrement = event => {
+  handleCardClick = event => {
     // Create a copy of the state.cards array as we don't want to modify state directly
     const prevCards = this.state.cards.slice();
 
@@ -144,7 +142,7 @@ class App extends Component {
         />
         <Header />
         <div className="container">
-          <Container cards={this.state.cards} onClick={this.handleIncrement} />
+          <Container cards={this.state.cards} onClick={this.handleCardClick} />
         </div>
         <Footer />
       </div>
